a9e707950874ff97c1f57653ec265ca93509950b
linkwk7 <linkwk7@outlook.com>
2018-09-14 20:24:37 +0800

    update

---
+++ 39.Combination Sum/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        std::sort(candidates.begin(), candidates.end());
+
+        std::vector<std::vector<int>> result;
+        std::vector<int> path;
+        combinationSumUtil(result, path, candidates, target,0);
+
+        return result;
+    }
+
+    void combinationSumUtil(std::vector<std::vector<int>> & result, std::vector<int>& path, const vector<int>& candidates, int target, int start) {
+        if (target == 0) {
+            result.push_back(path);
+        } else if (target < 0) {
+            return;
+        } else {
+            for (int i=start,sz=candidates.size(); i<sz; i++) {
+                path.push_back(candidates[i]);
+                combinationSumUtil(result, path, candidates, target-candidates[i], i);
+                path.pop_back();
+            }
+        }
+    }
+};
+
+int main() {
+    std::vector<int> data {10,1,2,7,6,1,5};
+
+    Solution s;
+    std::vector<std::vector<int>> result = s.combinationSum(data, 8);
+
+    std::for_each(result.begin(),result.end(), [](const std::vector<int> & arg) {
+        std::for_each(arg.begin(),arg.end(), [](int num) {
+            std::cout<<num<<",";
+        });
+        std::cout<<std::endl;
+    });
+}
