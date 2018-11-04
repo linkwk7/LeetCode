1713fd79c68c876538b1bc46e780e6cb96163b22
linkwk7 <linkwk7@outlook.com>
2018-11-04 20:32:45 +0800

    update

--- 75.Sort Colors/main.cpp
+++ 75.Sort Colors/main.cpp
@@ -7,24 +7,50 @@ using std::vector;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int one = 0;
-        int two = nums.size()-1;
+        // int one = 0;
+        // int two = nums.size()-1;
 
-        for (int i=0; i<=two; i++) {
-            if (nums[i] == 0) {
-                std::swap(nums[i], nums[one]);
-                one++;
-            } else if (nums[i] == 2) {
-                std::swap(nums[i], nums[two]);
-                two--;
-                i--;
+        // for (int i=0; i<=two; i++) {
+        //     if (nums[i] == 0) {
+        //         std::swap(nums[i], nums[one]);
+        //         one++;
+        //     } else if (nums[i] == 2) {
+        //         std::swap(nums[i], nums[two]);
+        //         two--;
+        //         i--;
+        //     }
+        // }
+
+        sortColorsAux(nums, 0, nums.size());
+    }
+
+    void sortColorsAux(std::vector<int> & nums, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+
+        int pivot = nums[l];
+        int eq = l;
+        int gt = l;
+        for (int i=l; i<r; i++) {
+            if (nums[i] < pivot) {
+                std::swap(nums[gt], nums[i]);
+                std::swap(nums[gt], nums[eq]);
+                eq++;
+                gt++;
+            } else if (nums[i] == pivot) {
+                std::swap(nums[gt], nums[i]);
+                gt++;
             }
         }
+
+        sortColorsAux(nums, l, eq);
+        sortColorsAux(nums, gt, r);
     }
 };
 
 int main() {
-    std::vector<int> data {2,0,1};
+    std::vector<int> data {9,3,5,4,1,7,9,8,8,1,4,6,2,7,0,0};
     Solution s;
     s.sortColors(data);
 
