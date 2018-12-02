e11e14026dfa2f83f573cce7040a14f460ba676e
linkwk7 <linkwk7@outlook.com>
2018-12-02 18:05:15 +0800

    update

--- 33.Search in Rotated Sorted Array/main.cpp
+++ 33.Search in Rotated Sorted Array/main.cpp
@@ -34,14 +34,14 @@ public:
         int left = begin;
         int right = end-1;
 
-        for (; left<right; ) {
+        for (; left<=right; ) {
             int mid = (left+right)/2;
             if (nums[mid] < target) {
                 left = mid+1;
             } else if (nums[mid] == target) {
                 return mid;
             } else {
-                right = mid;
+                right = mid-1;
             }
         }
         return (nums[left]==target ? left:-1);
